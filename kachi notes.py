590d753565963af754252af628965d58e9b208a6
nkfoss <fossx148@morris.umn.edu>
2019-10-02 22:04:46 -0500

    Implement connect/disconnect for row adjacent nodes

--- kachi notes.py
+++ kachi notes.py
@@ -44,8 +44,6 @@ class gridCell:
             currCell.isIsland = True
             currCell.maxBridges = maxBridges
             currCell.adjacentIslands = adjacentIslands
-            currCell.column = column
-            currCell.row = row
             currCell.isBridge = False
         else:
             currCell.isIsland = False
@@ -53,14 +51,125 @@ class gridCell:
             currCell.adjacentIslands = adjacentIslands
 
         currCell.isBridge = isBridge
-        adjacentIsland = []
-        currBridges = 0
+        currCell.adjacentIslands = []
+        currCell.connectedIslands = []
+        currCell.column = column
+        currCell.row = row
 
-    def connect(otherCell):
-        if otherCell.row != row or otherCell.column != column:
-            print("ERROR: Node not adjacent")
-            print( "Row: " + row + ". Column: " + column)
+    def setIsland(this, maxBridges):
+        this.maxBridges = maxBridges
+        this.isIsland = True
+
+    def currBridges(this):
+        return len(this.connectedIslands)
+
+
+
+
+    def connect(this, otherCell):
+
+        # Make sure the subject is an island
+        if (this.isIsland == False):
+            print("Subject is not an island.")
             return False
+
+        # Make sure target is an island
+        if (otherCell.isIsland == False):
+            print("Target is not an island.")
+            return False
+
+        # Make sure island doesn't connect to itself
+        if (this == otherCell):
+            print("Can't connect island to itself.")
+            return False
+
+        # Make sure we don't exceeed max connections
+        if (this.connectedIslands.count(otherCell) >= 2):
+            print("Can't connect again. Already have two connections.")
+            return False
+
+        # Make sure they are adjacent
+        if otherCell.row != this.row and otherCell.column != this.column:
+            print("ERROR: Node not adjacent")
+            print( "Subject Row: " + str(this.row) + ". Column: " + str(this.column) )
+            print( "Target  Row: " + str(otherCell.row) + ". Column: " + str(otherCell.column) )
+            return False
+
+        # If they're already connected once, go ahead do it again.
+        if (this.connectedIslands.count(otherCell) == 1):
+            otherCell.connectedIslands.append(this)
+            this.connectedIslands.append(otherCell)
+            print("Added a second connection.")
+            return True
+
+        # If they are adjacent, and in the same row...
+        if otherCell.row == this.row:
+            smallerColumn = min(this.column, otherCell.column)
+            largerColumn = max(this.column, otherCell.column)
+
+            # Now check nodes between them for obstacles
+            for column in range(smallerColumn+1, largerColumn):
+                currCell = gridColumns[column][this.row]
+                print(str(currCell.column) + " " + str(currCell.row) )
+                if (currCell.isIsland or currCell.isBridge):
+                    print("we hit something. can't connect")
+                    return False
+                else:
+                    currCell.isBridge = True
+
+            otherCell.connectedIslands.append(this)
+            this.connectedIslands.append(otherCell)
+            print("Connect succesful")
+            return True
+
+    def disconnect(this, otherCell):
+
+        if (this.connectedIslands.count(otherCell) < 1):
+            print("They aren't even connected.")
+            return False
+        if (this.connectedIslands.count(otherCell) == 2):
+            print("Disconnect success. 1 left.")
+            this.connectedIslands.remove(otherCell)
+            otherCell.connectedIslands.remove(this)
+            return True
+        else:
+            if otherCell.row == this.row:
+                smallerColumn = min(this.column, otherCell.column)
+                largerColumn = max(this.column, otherCell.column)
+                 # Now check nodes between them for obstacles
+                for column in range(smallerColumn+1, largerColumn):
+                    currCell = gridColumns[column][this.row]
+                    print(str(currCell.column) + " " + str(currCell.row) )
+                    if (currCell.isBridge):
+                        currCell.isBridge = False
+                        print("dismantling bridge")
+                    else:
+                        print("There should be a bridge here...but there isn't")
+                        print("Is it a bridge? " + currCell.isBrdige)
+                        print("Is it an island? " + currCell.isIsland)
+
+            print("Disconnect success. 0 left.")
+            this.connectedIslands.remove(otherCell)
+            otherCell.connectedIslands.remove(this)
+            return True
+
+
+
+
+    def makeBridge():
+        if isIsland == True:
+            print("Can't create bridge. This is an island")
+            return False
+        if isBridge == True:
+            print("This is already a bridge")
+            return False
+        else:
+            isBridge = True
+
+
+
+
+
 
 
 ### Now let's populate the grid itself.
@@ -68,13 +177,16 @@ class gridCell:
 n = 4
 gridColumns = []
 
-for i in range(0, 7):
+for i in range(0, n):
     gridColumns.append([])
-for i in range(0, 7):
-    for j in range(0, 7):
+for i in range(0, n):
+    for j in range(0, n):
         gridColumns[i].append(gridCell(False,
-        column = gridColumns[i], row=j)
+        column = i, row=j)
         )
+gridColumns[3][0].isIsland = True
+gridColumns[2][1].isIsland = True
+gridColumns[1][0].isIsland = True
 
 
 
