6465af4a84faae5083a200e9b9d66e4781394a13
Roen Aronson <arons121@morris.umn.edu>
2019-10-21 09:25:22 -0500

    Cleared up some error messages for connecting bridges

--- kachi notes.py
+++ kachi notes.py
@@ -122,6 +122,7 @@ class gridCell:
 
         # Make sure the island isn't full
         if this.maxBridges == 0:
+            print("Can't connect a full bridge")
             return False
 
         # Make sure they are adjacent
